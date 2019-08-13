11405b984ec1887932a521adf56b05ee56486cbc
Cameron Rossington <deoxys314@gmail.com>
2019-08-13 00:06:08 -0400

    New keymap for alpha.
    I'll fill this in later when I squash it, so no worries.
    
    TODO: image from keyboard layout editor.
    use layout for a few days
    check over README

---
+++ keyboards/alpha/keymaps/eagle/README.md
@@ -0,0 +1,43 @@
+![alpha layout image]()
+
+# Eagle layout for alpha
+
+Instead of rotating through layers using our extra R2 key like the default layout,
+this layout takes an aproach closer to the vderm layout.
+It is also heavily inspired by the Planck line of boards
+(which have become the _de facto_ standard for ortho boards,
+and so are familiar to most users.)
+
+## Layers
+
+There is some slight mnemonic thought put into the layers.
+The *NUMBER* layer is on the side of the keyboard with the `N` key.
+The *SYMBOL* layer is on the sidfe with the `S` key,
+and as a benefit, most symbols are on the right side of the board,
+where they are "expected" to be,
+easily accessible without hand contortions.]
+
+The two macros are included because they would be virtually impossible to use without contoprting one's hands.
+The `ctrl-alt-del` macro is on the `D` key, for `Delete`,
+and the `ctrl-shift-escape` macro is on the `A` key,
+mimicing where one's hands would normally be placed to use it.
+
+The `RESET` key is included because the author is incredibly tired of manually shorting pins.
+Though it loses out to the function keys as described below,
+it is placed beneath the `R` key (and by happy chance, the `F4` key),
+either of which should help useres remeber where it is located.
+
+## Concessions
+
+Owing primarily to the small size of the board, some concessions had to be made.
+
+The main impediment to usability is that there are no numperpad keys.
+For most this will be a minor issue,
+but some applications will only accept those specific keycodes.
+The author suggests a dedicated numberpad in these cases.
+
+The function keys are also not exactly in a row.
+In the author's experience, 
+most users key track of the function keys as being
+such-and-so a distance from the top left or top right corner.
+This relationship is maintained.
---
+++ keyboards/alpha/keymaps/eagle/keymap.c
@@ -0,0 +1,72 @@
+#include QMK_KEYBOARD_H
+
+/* DEFINES {{{ */
+
+/* HOME layer */
+#define ZCTL MT(MOD_LCTL, KC_Z)
+#define XGUI MT(MOD_LGUI, KC_X)
+#define CALT MT(MOD_LALT, KC_C)
+#define VSYM LT(SYMBOL,   KC_V)
+#define SSFT MT(MOD_LSFT, KC_SPC)
+#define BNUM LT(NUMBER,   KC_B)
+#define NCTL MT(MOD_RCTL, KC_N)
+#define MALT MT(MOD_RALT, KC_M)
+
+/* SYMBOL layer */
+#define COM_NUM LT(NUMBER, KC_COMM)
+
+/* control-alt-delete */
+#define CAD LCTL(LALT(KC_DEL))
+/* control-shift-esc */
+#define CSE LCTL(LSFT(KC_ESC))
+
+/* }}} */
+
+enum my_layers {
+    HOME,
+    NUMBER,
+    SYMBOL,
+    FUNCTION,
+};
+
+/* LAYERS {{{ */
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 
+     [LAYER] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+           _______, _______, _______, _______,       _______,    _______, _______, _______
+     ),
+     */
+     [HOME] = LAYOUT(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,     KC_K,    KC_L,    KC_ENT,
+           ZCTL,    XGUI,    CALT,    VSYM,          SSFT,       BNUM,    NCTL,    MALT
+     ),
+
+     [NUMBER] = LAYOUT(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+           _______, _______, _______, _______,       _______,    _______, _______, _______
+     ),
+
+     [SYMBOL] = LAYOUT(
+        KC_TAB,  KC_GRV,  _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_ESC, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_QUOT,
+           _______, _______, _______, _______,       _______,    COM_NUM, KC_DOT,  KC_SLSH
+     ),
+
+     [FUNCTION] = LAYOUT(
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        CSE,     _______, CAD,     RESET,   KC_F6,   KC_F7,   _______, _______, _______, KC_DEL,
+           _______, _______, _______, _______,       _______,    _______, _______, _______
+     ),
+
+};
+
+/* }}} */
+
+uint32_t layer_state_set_user(uint32_t state) {
+  return update_tri_layer_state(state, NUMBER, SYMBOL, FUNCTION);
+}
